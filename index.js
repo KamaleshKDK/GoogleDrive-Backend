65cf98a998c19f1a1c1ce2f4a72c2437133932ca
kamaleshKDK <kamaleshkdk@gmail.com>
2022-01-19 13:26:55 +0530

    verify changed

--- index.js
+++ index.js
@@ -82,6 +82,14 @@ app.post("/login", async function (req, res) {
     }
 })
 
+var sender = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "studypurposedemo2021@gmail.com",
+        pass: "practicedummy@123"
+    }
+});
+
 
 //Customer registration form 
 
--- verify.js
+++
@@ -1,7 +0,0 @@
-var sender = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "studypurposedemo2021@gmail.com",
-        pass: "practicedummy@123"
-    }
-});
